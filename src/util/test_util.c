9b05a297c0b6b38f31299fd6b92f9b48a6ad103a
drdr xp <drdr.xp@gmail.com>
2017-09-25 01:57:07 +0800

    test inc/util.h

---
+++ src/util/test_util.c
@@ -0,0 +1,59 @@
+#include "inc/inc.h"
+#include "unittest/unittest.h"
+
+st_test(util, st_nelts) {
+
+    int n;
+
+    { int arr[10];       n = st_nelts(arr); ddx(n); st_ut_eq(10, n, ""); }
+    { int *arr[10];      n = st_nelts(arr); ddx(n); st_ut_eq(10, n, ""); }
+    { int arr[10][10];   n = st_nelts(arr); ddx(n); st_ut_eq(10, n, ""); }
+    { void *arr[10][10]; n = st_nelts(arr); ddx(n); st_ut_eq(10, n, ""); }
+}
+
+st_test(util, st_unused) {
+    int n;
+    /* no warning */
+    st_unused(n);
+}
+
+st_test(util, st_align) {
+
+    struct case_s {
+        uint64_t inp;
+        uint64_t upto;
+        uint64_t expected;
+    } cases[] = {
+        {0, 0, 0},
+        {0, 1, 0},
+        {0, 2, 0},
+        {1, 2, 2},
+        {2, 2, 2},
+        {0, 4, 0},
+        {1, 4, 4},
+        {2, 4, 4},
+        {3, 4, 4},
+        {4, 4, 4},
+    };
+
+    for (int i = 0; i < st_nelts(cases); i++) {
+        st_typeof(cases[0])   c = cases[i];
+        st_typeof(c.expected) rst = st_align(c.inp, c.upto);
+
+        ddx(rst);
+
+        st_ut_eq(c.expected, rst, "");
+    }
+}
+
+/*
+ * TODO
+ *     offset
+ *     by_offset
+ *     owner
+ *     min
+ *     max
+ *     foreach
+ */
+
+st_ut_main;
