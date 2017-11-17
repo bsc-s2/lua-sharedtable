225310884dde78a3cd609929ff857dda46b41de4
Li, Shulong <shulong.li@baishancloud.com>
2017-11-17 16:58:08 +0800

    add build compile varibale in def.mk

--- src/def.mk
+++ src/def.mk
@@ -3,15 +3,66 @@
 # $@ target
 
 # usage:
-#     make ** DEBUG=1 DEBUG_UNITTEST=1
+#     make ** build=release|test debug=0|1 debug_unittest=0|1 mem=small|normal
+#
+DEBUG_FLAGS_1 = -fprofile-arcs -ftest-coverage -g -D ST_DEBUG -O2 -D DNDEBUG
+UNITTEST_FLAGS_1 = -D ST_DEBUG_UNITTEST
+MEM_FLAGS_normal = -D MEM_NORMAL
+MEM_FLAGS_small = -D MEM_SMALL
 
-DEBUG_FLAGS    = $(if $(DEBUG), -fprofile-arcs -ftest-coverage -g -D ST_DEBUG, -O2 -D DNDEBUG)
-# DEBUG_FLAGS    = $(if $(DEBUG), -fprofile-arcs -ftest-coverage -g , -O2 -D DNDEBUG)
-UNITTEST_FLAGS = $(if $(DEBUG_UNITTEST), -D ST_DEBUG_UNITTEST)
+ifndef build
+	build = release
+endif
+
+BUILD_TYPES = release test
+ifeq ($(findstring $(build), $(BUILD_TYPES)),)
+$(error unknown build type, valid value: $(BUILD_TYPES))
+endif
+
+ifndef debug
+	ifeq ($(build), release)
+		debug = 0
+	else
+		debug = 1
+	endif
+endif
+
+ifndef debug_unittest
+	ifeq ($(build), release)
+		debug_unittest = 0
+	else
+		debug_unittest = 1
+	endif
+endif
+
+ifndef mem
+	ifeq ($(build), release)
+		mem = normal
+	else
+		mem = small
+	endif
+endif
+
+ifeq ($(findstring $(debug), 0 1),)
+$(error invalid debug value $(debug), on: 1, off: 0)
+endif
+
+ifeq ($(findstring $(debug_unittest), 0 1),)
+$(error invalid debug value $(debug_unittest), on: 1, off: 0)
+endif
+
+ifeq ($(findstring $(mem), small normal),)
+$(error invalid debug value $(mem), valid value: small normal)
+endif
+
+BUILD_EXTRA_CFLAGS += $(DEBUG_FLAGS_$(debug))
+BUILD_EXTRA_CFLAGS += $(UNITTEST_FLAGS_$(debug_unittest))
+BUILD_EXTRA_CFLAGS += $(MEM_FLAGS_$(mem))
+
 
 CC        = gcc
 BASE_DIR ?= ..
-CFLAGS   ?= -g -Wall -Werror -std=c11 -D_GNU_SOURCE $(DEBUG_FLAGS)
+CFLAGS   ?= -g -Wall -Werror -std=c11 -D _GNU_SOURCE $(BUILD_EXTRA_CFLAGS)
 
 C_INCLUDE_PATH = $(BASE_DIR):$(BASE_DIR)/inc
 export C_INCLUDE_PATH
@@ -69,6 +120,6 @@ $(test_objs): $(BASE_DIR)/inc/*.h $(BASE_DIR)/unittest/*.h
 	ar -rcs $@ $(objs)
 
 .c.o:
-	$(CC) $(CFLAGS) $(cflags) $(UNITTEST_FLAGS) -o $@ -c $<
+	$(CC) $(CFLAGS) $(cflags) -o $@ -c $<
 
 .h.c:
