8054a0a6db5c79cd3bd988a57e26c332d048072f
drdr xp <drdr.xp@gmail.com>
2017-12-13 13:29:54 +0800

    use __auto_type in gcc

--- src/inc/bit.h
+++ src/inc/bit.h
@@ -4,7 +4,7 @@
 /* count leading zero of an unsigned int */
 /* __builtin_clz does not specify result of __builtin_clz(0) */
 #define st_bit_clz(i) ({                                                      \
-        st_typeof(i) clz_i_ = (i);                                            \
+        __auto_type clz_i_ = (i);                                             \
         (clz_i_) == 0 ? sizeof(clz_i_) * 8 : st_bit_clz_positive_int_(clz_i_);\
 })
 
@@ -25,7 +25,7 @@
  * for i>0, msb satisfies: 2^i == 1 << msb(i)
  */
 #define st_bit_msb(i) ({                                                      \
-        st_typeof(i) msb_i_ = (i);                                            \
+        __auto_type msb_i_ = (i);                                             \
         (sizeof(msb_i_) * 8 - st_bit_clz(msb_i_) - 1);                        \
 })
 
