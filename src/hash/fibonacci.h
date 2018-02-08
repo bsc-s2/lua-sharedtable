5e1163871a1effb1538378c868442b8f8be78348
张炎泼 <drdr.xp@gmail.com>
2018-02-08 13:15:19 +0800

    format fibonacci.sh

--- src/hash/fibonacci.h
+++ src/hash/fibonacci.h
@@ -1,31 +1,36 @@
-#ifndef __HASH__FIB_HASH_H__
-#     define __HASH__FIB_HASH_H__
+#ifndef HASH__FIBONACCI_H
+#define HASH__FIBONACCI_H
 
 #include <stdint.h>
 
+/*
+ *          √5 - 1
+ * golden = ------- =~ 0.618
+ *             2
+ */
 
-    static inline uint16_t
-fib_hash16( uint16_t i )
-{
+
+static inline uint16_t
+fib_hash16(uint16_t i) {
+    /* golden * (1<<16) */
     return i * 40503;
 }
 
-    static inline uint32_t
-fib_hash32( uint32_t i )
-{
+static inline uint32_t
+fib_hash32(uint32_t i) {
+    /* golden * (1<<32) */
     return i * 2654435769;
 }
 
-    static inline uint64_t
-fib_hash64( uint64_t i )
-{
+static inline uint64_t
+fib_hash64(uint64_t i) {
+    /* golden * (1<<64) */
     return i * 11400714819323198485ull;
 }
 
-    static inline uint8_t
-fib_hash8( uint8_t i )
-{
-    return ( uint8_t )fib_hash16( i );
+static inline uint8_t
+fib_hash8(uint8_t i) {
+    return (uint8_t)fib_hash16(i);
 }
 
-#endif /* __HASH__FIB_HASH_H__ */
+#endif /* HASH__FIBONACCI_H */
